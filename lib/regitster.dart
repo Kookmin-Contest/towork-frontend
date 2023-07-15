f32143ffd8d0b1bf198b347029fa2c6c60146bea
Park Junseok <65781023+jjunseoki@users.noreply.github.com>
2023-07-15 20:02:04 +0900

    Add files via upload
    register_page

---
+++ lib/regitster.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '회원가입 예제',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('회원가입'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                labelText: '이메일',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '이메일을 입력해주세요.';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock),
+                labelText: '비밀번호',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '비밀번호를 입력해주세요.';
+                }
+                return null;
+              },
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // 이 부분에 회원가입 로직을 추가해주세요.
+                  // 예를 들어, Firebase를 사용한다면
+                  // FirebaseAuth.instance.createUserWithEmailAndPassword(...)
+                }
+              },
+              child: Text('회원가입'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
