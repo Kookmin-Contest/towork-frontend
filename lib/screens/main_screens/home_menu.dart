2a362687f0d1aad0489923ec3be6ca21dc4fa801
Park Junseok <65781023+jjunseoki@users.noreply.github.com>
2023-08-08 19:50:04 +0900

    Update home_menu.dart
    3rd_version

--- lib/screens/main_screens/home_menu.dart
+++ lib/screens/main_screens/home_menu.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gotowork/main.dart';
-import 'package:gotowork/screens/login_screens/login_menu.dart';
 import 'package:gotowork/widgets/calendar.dart';
 
 class HomeMenu extends StatefulWidget {
@@ -23,21 +22,62 @@ class _HomeMenuState extends State<HomeMenu> {
             child: Column(
               children: [
                 SizedBox(height: 40.0),
-                ElevatedButton(
-                  onPressed: () async {
-                    await storage.delete(key: 'login');
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyApp()),
-                      (route) => false,
-                    );
-                  },
-                  child: Text('임시 로그아웃 버튼'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 350.0,
+                      height: 100.0,
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(color: Colors.grey, width: 2.0),
+                      ),
+                      child: Stack(
+                        children: [
+                          // 텍스트
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: RichText(
+                              textAlign: TextAlign.left,
+                              text: TextSpan(
+                                style: DefaultTextStyle.of(context).style,
+                                children: <TextSpan>[
+                                  TextSpan(text: '4/21일(금)의 근무시간은\n'),
+                                  TextSpan(
+                                      text: '21:00 ~ 06:00',
+                                      style: TextStyle(fontSize: 20)),
+                                  TextSpan(text: '입니다.'),
+                                ],
+                              ),
+                            ),
+                          ),
+                          // 로그아웃 버튼
+                          Positioned(
+                            right: 0,
+                            bottom: 0,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                await storage.delete(key: 'login');
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MyApp()),
+                                  (route) => false,
+                                );
+                              },
+                              child: Text('임시 로그아웃 버튼'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 6.0),
+                SizedBox(height: 20.0),
                 Container(
-                  width: 360.0,
-                  height: 350.0,
+                  width: 350.0,
+                  height: 400.0,
                   padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(40.0),
